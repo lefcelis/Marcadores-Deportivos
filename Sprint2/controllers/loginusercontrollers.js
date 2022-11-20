043d52ca31a63259be82a71a203bbcd6f2f76dc3
Maria Camila Esquivel Alvarez <116690768+mariacamilaea@users.noreply.github.com>
2022-11-20 15:53:15 -0500

    Backend

---
+++ Sprint2/controllers/loginusercontrollers.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import { regusuario, showuser, shuserone, deluser, upuser  } from '../controllers/userscontroller.js'
+import { regevento, showevento, showonevento, delonevent, upevento} from '../controllers/eventscontollers.js';
+import {db} from '../mongodb.js';
+
+export const router = express.Router();
+
+//rutas collection eventos
+
+router.post('/regevento', regevento);
+router.get('/shevento', showevento);
+router.get('/shevento/:id', showonevento);
+router.delete('/delevento/:id', delonevent);
+router.put('/upevento/:id', upevento);
+
+//rutas collection usuarios de la bd de mongodb
+router.post('/reguser', regusuario);
+router.get('/shuser', showuser);
+router.get('/shuser/:id', shuserone);
+router.delete('/deluser/:id',deluser);
+router.put('/upuser/:id', upuser);
+
+export default router;
